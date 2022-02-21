0b43a8f258dd90e83ddcf9ab7562e8230a5bb2ce
BANGKOTEX <99166127+BANGKOTEX@users.noreply.github.com>
2022-02-21 13:48:17 +0700

    membuat switch

---
+++ Switch.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Warna favoritmu adalah merah!";
+    break;
+  case "blue":
+    echo "Warna favoritmu adalah biru!";
+    break;
+  case "green":
+    echo "Warna favoritmu adalah hijau!";
+    break;
+  default:
+    echo "Your favorite color is neither merah, viru, atau hijau!";
+}
+?>
+ 
+</body>
+</html>
